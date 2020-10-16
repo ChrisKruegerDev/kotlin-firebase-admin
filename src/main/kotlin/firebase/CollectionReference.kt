b417133229abd25a41d461fa52b2c231001475a9
chris <15275971+chrisbln@users.noreply.github.com>
2020-10-16 14:18:53 +0200

    Add method to CollectionReference

--- src/main/kotlin/firebase/CollectionReference.kt
+++ src/main/kotlin/firebase/CollectionReference.kt
@@ -1,5 +1,9 @@
 package firebase
 
+import kotlin.js.Promise
+
 external interface CollectionReference : Query {
+    fun doc(): DocumentReference
     fun doc(documentPath: String): DocumentReference
+    fun add(data: dynamic): Promise<DocumentReference>
 }
