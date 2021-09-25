8887d23e1d6964783d25056e3ba1b0e66af150b7
Christian Krüger <15275971+chrisnkrueger@users.noreply.github.com>
2021-09-25 17:15:47 +0200

    Bump versions

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,9 +1,9 @@
 object Versions {
     val versionMajor = 1
     val versionMinor = 4
-    val versionPatch = 0
+    val versionPatch = 1
 
     val versionName = "$versionMajor.$versionMinor.$versionPatch"
-    val kotlin = "1.4.32"
+    val kotlin = "1.5.30"
     val nexus = "1.0.0"
 }
